233fa6cd21aba41fde0f498fa60152217d7f6f46
adamnew123456 <adamnew123456@gmail.com>
2014-06-15 12:11:49 -0400

    The device loader stub now handles named devices.

--- cuos/dev-stub.lua
+++ cuos/dev-stub.lua
@@ -2,27 +2,34 @@
 A stub which loads all the entries in the /dev filesystem.
 --]]
 
-SIDES = {'left', 'right', 'top', 'bottom', 'front', 'back'}
+SIDES = {left='L', right='R', top='T', bottom='U', front='F', back='B'}
 
 -- Clean out the directory structure of /dev
 fs.delete('/dev')
 
 fs.makeDir('/dev')
 fs.makeDir('/dev/peripherals')
-for _, side in pairs(SIDES) do
+for side, short_side in pairs(SIDES) do
     local handle = fs.open('/dev/peripherals/' .. side, 'w')
     local content = [==[
 return nil
 ]==]
 
-    if peripheral.getType(side) ~= nil then
+    local peripheral_type = peripheral.getType(side)
+    if peripheral_type ~= nil then
         content = string.format([==[
 return {
     side = "%s",
     type = "%s",
     methods = peripheral.wrap("%s")
 }
-]==], side, peripheral.getType(side), side)
+]==], side, peripheral_type, side)
+
+        -- Added the named version of the peripheral
+        local named_peripheral = fs.open('/dev/' .. peripheral_type 
+                .. short_side, 'w')
+        named_peripheral.write(content)
+        named_peripheral.close()
     end
 
     handle.write(content)
